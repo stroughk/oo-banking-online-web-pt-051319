4c437be4651ea489d8389216ba6730c05a5bfaff
Kateryna Strough <katerynastrough@hotmail.com>
2019-06-08 19:11:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,12 @@
 class Transfer
-  
-def initialize
-  
-end 
+  attr_reader :amount, :sender, :receiver
+  attr_accessor :status
+
+  def initialize(sender, receiver, amount)
+    @status = "pending"
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+  end
   
 end
