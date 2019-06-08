955ec67f376161da4a1e85e5c4447f99f4bf741f
Kateryna Strough <katerynastrough@hotmail.com>
2019-06-08 18:50:59 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,10 +3,10 @@ class BankAccount
   attr_accessor  :name, :balance
   
 
-def initialize(name, balance=1000, status=="open")
+def initialize(name, balance=1000)
   @name = name 
   @balance = balance
-  @status = status 
+
   
 end 
 
