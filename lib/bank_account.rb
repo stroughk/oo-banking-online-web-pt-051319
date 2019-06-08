61870cf3098722c0c5f19a563e912af3798b7bd0
Kateryna Strough <katerynastrough@hotmail.com>
2019-06-08 18:50:37 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,13 @@
 class BankAccount
+  
+  attr_accessor  :name, :balance
+  
+
+def initialize(name, balance=1000, status=="open")
+  @name = name 
+  @balance = balance
+  @status = status 
+  
+end 
 
 end
